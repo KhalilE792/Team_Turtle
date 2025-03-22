dc8fc51b6c5fc280f0066fedeb78d7b9302537ac
Vinh Le <vinhleat@gmail.com>
2025-03-22 17:34:53 -0500

    Organized Users into MongoDB model.

--- login.js
+++ login.js
@@ -4,27 +4,20 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
+
 // PASSWORD STUFF
-const bcrypt = require('bcrypt')
+const bodyParser = require('body-parser')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+
 // MONGOOSE SERVER
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database!!!'))
-
-const initializePassport = require('./passport-config')
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
-
-const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -38,62 +31,11 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-})
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch {
-        res.redirect('/register')
-    }
-})
-
-app.post('/logout', function (req, res, next) {
-    req.logout(function (err) {
-        if (err) { return next(err) }
-        res.redirect('/')
-    })
-})
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-
-    res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/')
-    }
-    next()
-}
-
+app.use(bodyParser.json());
 app.use(express.json())
+
+const usersRouter = require('./routes/users')
+app.use('/', usersRouter)
 
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
---
+++ routes/users.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+const initializePassport = require('../passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = []
+
+router.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
+
+router.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+router.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+router.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+        console.log('Did not input new user')
+    }
+})
+
+router.post('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err) }
+        res.redirect('/')
+    })
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+module.exports = router
